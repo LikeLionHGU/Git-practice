d0d9f63d7818811074ee779176383ca87b98d517
KimkyoungJins <kkjin722@gmail.com>
2024-01-18 16:36:38 +0900

    Feat: printByJin 메소드구현

--- src/main/java/org/example/MessagePrinter.java
+++ src/main/java/org/example/MessagePrinter.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class MessagePrinter {
 
-    public void printByYourName() {
-        System.out.println("Hello, my name is YourName");
+    public void printByJin() {
+        System.out.println("Hello, my name is Jin");
     }
 
     // TODO: 자신의 이름을 출력하는 printBy자신의이름() 메서드를 구현하시오
