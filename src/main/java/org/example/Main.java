9f33c630a2b85c735ceaaa3941060cb9c44837bc
JuanHubb <wngks1350@gmail.com>
2024-12-27 20:44:58 +0900

    [13기] 홍주한 테스트 커밋 작성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+ckdldkdknfkawofnanfanfgalmnf12193838383848484
         MessagePrinter messagePrinter = new MessagePrinter();
     }
 }
