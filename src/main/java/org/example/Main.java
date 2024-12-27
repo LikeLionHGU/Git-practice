3880a55100abb6d0b5db104675f1004b9fe633a8
JSH-631 <newred02@handong.ac.kr>
2024-12-27 20:44:38 +0900

    [13기] 장세혁 테스트 커밋 작성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-        MessagePrinter messagePrinter = new MessagePrinter();
+	    MessagePrinter messagePrinter = new MessagePrinter ();
+	    1234
     }
 }
